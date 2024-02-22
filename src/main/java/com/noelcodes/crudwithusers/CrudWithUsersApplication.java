6d27e674b0099e1db1bb65e49d0a9e65e0d61abd
iamdevnoel <contato.gabrielnoberto@gmail.com>
2024-02-22 20:06:38 -0300

    Create project

---
+++ src/main/java/com/noelcodes/crudwithusers/CrudWithUsersApplication.java
@@ -0,0 +1,13 @@
+package com.noelcodes.crudwithusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noelcodes/crudwithusers/CrudWithUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelcodes.crudwithusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
